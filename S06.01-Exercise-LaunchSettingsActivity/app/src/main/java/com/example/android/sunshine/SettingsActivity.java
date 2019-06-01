a2d349a2564385656a79014f3a97ecc85c283078
Yini <ydxb7@mail.missouri.edu>
2019-06-01 03:55:45 -0700

    S06.01 创建一个空的 SettingsActivity 并通过 Sunshine 的 ActionBar 菜单打开它。

---
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -0,0 +1,34 @@
+package com.example.android.sunshine;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+// TODO (1) Add new Activity called SettingsActivity using Android Studio wizard
+public class SettingsActivity extends AppCompatActivity {
+
+    // TODO (2) Set setDisplayHomeAsUpEnabled to true on the support ActionBar
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        ActionBar actionBar = this.getSupportActionBar();
+
+        // Set the action bar back button to look like an up button
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
