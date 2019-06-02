a4ec97c9eae3ecf3eee4475662bb2cf73ca2d4a1
Yini <ydxb7@mail.missouri.edu>
2019-06-01 18:29:21 -0700

    S06.03 偏好设置温度单位，当输入新地址后，打开地图会自动显示新地址-PolishingPreferences

--- S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -16,6 +16,12 @@
 package com.example.android.sunshine.data;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceManager;
+
+import com.example.android.sunshine.MainActivity;
+import com.example.android.sunshine.R;
 
 public class SunshinePreferences {
 
@@ -89,7 +95,11 @@ public class SunshinePreferences {
     public static String getPreferredWeatherLocation(Context context) {
         // TODO (1) Return the user's preferred location
         /** This will be implemented in a future lesson **/
-        return getDefaultWeatherLocation();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String keyForLocation = context.getString(R.string.pref_location_key);
+        String defaultLocation = context.getString(R.string.pref_location_default);
+        String location = sharedPreferences.getString(keyForLocation, defaultLocation);
+        return location;
     }
 
     /**
@@ -102,7 +112,12 @@ public class SunshinePreferences {
     public static boolean isMetric(Context context) {
         // TODO (2) Return true if the user's preference for units is metric, false otherwise
         /** This will be implemented in a future lesson **/
-        return true;
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String keyForUnits = context.getString(R.string.pref_units_key);
+        String defaultUnits = context.getString(R.string.pref_units_metric);
+        String units = sharedPreferences.getString(keyForUnits, defaultUnits);
+
+        return units.equals(defaultUnits);
     }
 
     /**
